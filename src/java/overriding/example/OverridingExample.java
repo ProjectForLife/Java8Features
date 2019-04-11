66c3014243c126c27b5ecd767fa3ff14a3011c33
hemanth kumar <mailtohemanthkumar@gmail.com>
2019-04-11 22:53:54 +0530

    changes for overriding functionality

--- src/java/overriding/example/OverridingExample.java
+++ src/java/overriding/example/OverridingExample.java
@@ -1,5 +1,8 @@
 package overriding.example;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class OverridingExample {
 
 	public static void main(String[] args) {
@@ -13,6 +16,8 @@ public class OverridingExample {
 		subclass.method2();
 		superclass.method2();
 		holdingSubClass.method2();
+		subclass.method4();
+		
 		
 	}
 
@@ -26,6 +31,23 @@ class SuperClass{
 	void method2() {
 		System.out.println("I am from superclass method2");
 	}
+	
+	void method3() {
+		System.out.println("I am from superclass method3");
+	}
+	
+	private void method4() {
+		System.out.println("I am from superclass method4");
+	}
+	
+	public Object method5() {
+		return "";
+	}
+	
+	public Object method6() throws IOException{
+		return "";
+	}
+	
 }
 
 class SubClass extends SuperClass{
@@ -36,4 +58,31 @@ class SubClass extends SuperClass{
 	void method2() {
 		System.out.println("I am from subclass method2");
 	}
+	
+	//Cannot reduce the visibility of the inherited method from SuperClass
+	/*private void method3() { 
+		System.out.println("I am from superclass method3");
+	}*/
+	
+	void method4() {
+		System.out.println("I am from subclass method4");
+	}
+	
+	//The return type must be the same or a more restrictive type, also known as covariant
+	//return types.
+	public String method5() {
+		return "";
+	}
+	
+//If any checked exceptions are thrown, only the same exceptions or subclasses of those
+	//exceptions are allowed to be thrown.
+	/*public Object method6() throws Exception {
+		return "";
+	}*/
+	
+	public Object method6() throws FileNotFoundException {
+		return "";
+	}
+	
+	
 }
